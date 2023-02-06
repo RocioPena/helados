37152b0ee0af097b87e835249a8ada98342f1c0e
RocioPena <78064085+RocioPena@users.noreply.github.com>
2023-02-06 00:28:35 -0600

    apartados de helado, frappe y paletas casi listo
    lo que falta es que aparezca un recuadro donde aparezca el pedido, y como se puede ir almacenando y sacar la cuenta total de dichos productos que pidio el cliente

---
+++ heladeria/static/js/acciones.js
@@ -0,0 +1,73 @@
+function mostrarHelado(){
+    document.getElementById('apartadoHelado').style.display = 'block'
+    document.getElementById('apartadoPaletas').style.display = 'none'
+    document.getElementById('apartadoFrappe').style.display = 'none'
+}
+function mostrarPaletas(){
+    document.getElementById('apartadoPaletas').style.display = 'block'
+    document.getElementById('apartadoHelado').style.display = 'none'
+    document.getElementById('apartadoFrappe').style.display = 'none'
+}
+function mostrarFrappe(){
+    document.getElementById('apartadoFrappe').style.display = 'block'
+    document.getElementById('apartadoHelado').style.display = 'none'
+    document.getElementById('apartadoPaletas').style.display = 'none'
+}
+function eliminarOpcion(){
+    document.getElementById('apartadoHelado').style.display = 'none'
+    document.getElementById('apartadoPaletas').style.display = 'none'
+    document.getElementById('apartadoFrappe').style.display = 'none'
+}
+
+
+function mostrarClasico(){
+    document.getElementById('heladoClasico').style.display = 'block'
+    document.getElementById('heladoEspecial').style.display = 'none'
+}
+function mostrarEspecial(){
+    document.getElementById('heladoEspecial').style.display = 'block'
+    document.getElementById('heladoClasico').style.display = 'none'
+}
+
+
+function mostrarPaletaEspecial(){
+    document.getElementById('paletaEspecial').style.display = 'block'
+    document.getElementById('paletaLeche').style.display = 'none'
+    document.getElementById('paletaAgua').style.display = 'none'
+}
+function mostrarPaletaLeche(){
+    document.getElementById('paletaLeche').style.display = 'block'
+    document.getElementById('paletaEspecial').style.display = 'none'
+    document.getElementById('paletaAgua').style.display = 'none'
+}
+function mostrarPaletaAgua(){
+    document.getElementById('paletaAgua').style.display = 'block'
+    document.getElementById('paletaLeche').style.display = 'none'
+    document.getElementById('paletaEspecial').style.display = 'none'
+}
+
+
+
+function mostrarFrappeLeche(){
+    document.getElementById('frappeLeche').style.display = "block"
+    document.getElementById('frappeAgua').style.display = "none"
+    document.getElementById('frappeHelado').style.display = "none"
+}
+function mostrarFrappeAgua(){
+    document.getElementById('frappeAgua').style.display = "block"
+    document.getElementById('frappeLeche').style.display = "none"
+    document.getElementById('frappeHelado').style.display = "none"
+}
+function mostrarFrappeHelado(){
+    document.getElementById('frappeHelado').style.display = "block"
+    document.getElementById('frappeAgua').style.display = "none"
+    document.getElementById('frappeLeche').style.display = "none"
+}
+function mostrarClasico2(){
+    document.getElementById('heladoClasico2').style.display = 'block'
+    document.getElementById('heladoEspecial2').style.display = 'none'
+}
+function mostrarEspecial2(){
+    document.getElementById('heladoEspecial2').style.display = 'block'
+    document.getElementById('heladoClasico2').style.display = 'none'
+}
